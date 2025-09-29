d9e3b7fed8016f0b518bd3f577d36db7518a3d37
Shivansh-Rai <shivanshrai2316@gmail.com>
2025-09-29 11:18:04 +0530

    new

---
+++ ARMSTRONG NUMBER 1.cpp
@@ -0,0 +1,20 @@
+//ARMSTRONG NUMBER
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    for (int i=a;i<b;i++){
+        string s=to_string(i);
+        int l=s.size();
+        int sum=0;
+        for (char j : s){
+            int num = j-'0'; //convert string to integer
+            sum+= pow(num,l);
+        }
+        if (sum==i){
+            cout<<sum<<" ";
+        }
+    }
+}
